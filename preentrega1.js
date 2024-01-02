27ad9235bf0eb55725f54a44d2bc459469a4bcfa
mcpablo <pablomconturso@gmail.com>
2024-01-02 10:57:27 -0300

    preentrega1

--- preentrega1.js
+++ preentrega1.js
@@ -2,9 +2,13 @@
 
 // 1) Ingreso al sistema a traves de un usuario y contraseña. Si se repite mas de 3 veces un error el usuario será bloqueado y deberá dirigirse a administracion para su desbloqueo.
 
-// 2) Resultados de analisis clinicos del paciente. Aqui ciertos valores deberan estar dentro de lo esperable para poder administrar la droga. Si algun valor no cumplira dicho requisito, deberá avisarle al médico para que evalue como proceder.
+// 2) Resultados de analisis clinicos del paciente. Aqui ciertos valores deberan estar dentro de lo esperable para poder administrar la droga. Si algun valor no cumplira dicho requisito, deberá avisarle al médico para que evalue como proceder. Valores a considerar:
+
+// globulos blancos >= 
+
 
 //3) Si todos los valores anteriores estan dentro de lo esperable, SI podra aceptarse la aplicacion la medicacion, de lo contrario NO se podrá.
+
 let usuario = parseFloat(prompt('Ingrese Usuario'));
 let contraseña = prompt('Ingrese su contraseña');
 let mensaje;
@@ -12,21 +16,14 @@ let errorIngreso = 0;
 
 if (usuario == 'drmengele' && contraseña == '123abc') {
     mensaje = ('Bienvenido ') + usuario;
-}
-else {
+} else {
     mensaje = 'Usuario o contraseña incorrecta ❌';
+    errorIngreso = errorIngreso + 1;
 }
-
-while (usuario != 'drmengele' && contraseña != '123abc') {
-    console.log('Usuario invalido');
-    usuario = prompt('Usuario');
-}
-
 
 for (let errorIngreso = 0; errorIngreso >= 3; errorIngreso++) {
     alert('Usuario bloqueado, dirigirse a Administración')
 
 }
-alert(mensaje)
 
 
